bf2cd6c1d67383debca075fb2a63dcf36953cec9
Michael Wu <mw10013@gmail.com>
2023-05-12 20:33:48 -0400

    add react createRoot

--- web/frontend/index.jsx
+++ web/frontend/index.jsx
@@ -1,5 +1,11 @@
-import ReactDOM from "react-dom";
-
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+// https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
+
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// ReactDOM.render(<App />, document.getElementById("app"));
